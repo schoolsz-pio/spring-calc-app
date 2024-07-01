05d17807e16e436fe54b8322b428abecad284f38
Piotr Szotek <piotr.szotek13441@stud.wst.pl>
2024-07-01 22:40:39 +0200

    feat: Allow form to create user

--- src/main/java/com/example/calc/controllers/AuthController.java
+++ src/main/java/com/example/calc/controllers/AuthController.java
@@ -4,9 +4,19 @@ package com.example.calc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.example.calc.controllers.UserController ;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthController {
+
+
+  @Autowired
+  private UserController uController;
 
   @GetMapping("/login")
   public String loginForm(Model model) {
@@ -15,5 +25,19 @@ public class AuthController {
   @GetMapping("/register")
   public String registerForm(Model model) {
     return "register"; 
+  }
+  @PostMapping("/register")
+  public String registerSubmit(
+    @RequestParam("email") String email, @RequestParam("name") String name
+    ,Model model) {
+    try  {
+      // TODO: Check if it is a error
+      String resp = uController.addNewUser(name, email);
+      model.addAttribute("registermessage", resp);
+      return "login";
+    } catch (Exception e) {
+      return "index";
+
+    }
   }
 }
