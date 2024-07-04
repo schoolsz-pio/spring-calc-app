0ec7ae55b431d43bd65d93342bf86d2412713f39
Piotr Szotek <piotr.szotek13441@stud.wst.pl>
2024-07-04 20:46:20 +0200

    feat: Add option to add calculation from website

--- src/main/java/com/example/calc/controllers/IndexController.java
+++ src/main/java/com/example/calc/controllers/IndexController.java
@@ -8,11 +8,22 @@ import com.example.calc.repositories.UserRepository;
 import com.example.calc.entities.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+
+import com.example.calc.entities.Calculation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.example.calc.controllers.CalculationController;
+import java.util.ArrayList;
+import java.util.List;
 @Controller
 public class IndexController {
 
   @Autowired 
   private UserRepository userRepository;
+
+  @Autowired
+  private CalculationController calculationservice;
 
   private User getUser(){
 
@@ -34,6 +45,8 @@ public class IndexController {
         loggedIn = true;
         String str = user.getName();
         model.addAttribute("name", str);
+
+        model.addAttribute("calculations",calculationservice.getCalculations(user.getId()));
       }
     } catch (Exception e) {
       System.out.println("Invalid integer input");
@@ -45,6 +58,32 @@ public class IndexController {
   public String home(Model model) {
     giveBetterName(model);
     return "index"; 
+  }
+
+
+  @PostMapping("/")
+  public String homeForm(@RequestParam("expression") String expression, Model model) {
+
+    try  {
+      User user = getUser();
+
+      calculationservice.addCalculation(user.getId(), expression);
+    } catch (Exception e) { 
+      try {
+
+        Calculation calculation = calculationservice.addCalculation( expression);
+        List<Calculation> list = new ArrayList<>();
+        list.add(calculation);
+
+
+        model.addAttribute("calculations", list);
+      } catch (Exception f) {
+        System.err.println(f);
+        return home(model);
+      }
+    }
+
+    return home(model);
   }
 
 
